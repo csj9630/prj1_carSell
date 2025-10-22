2983d44e12048372581867332dcbcc5ed32de1b4
csjSist <csj9630@gmail.com>
2025-10-22 10:35:11 +0900

    최승준 : 커밋3

--- prj1_carSell/src/kr/co/sist/csj/Csj.java
+++ prj1_carSell/src/kr/co/sist/csj/Csj.java
@@ -5,5 +5,6 @@ public class Csj {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("CSI Miami season2");
+		System.out.println("CSI Miami season3");
 	}//main
 }//class
