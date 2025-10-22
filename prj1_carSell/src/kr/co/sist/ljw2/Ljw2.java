955172ab53a22aa5d4c7f376b45bc9d7d830654e
doraemong <doraemong@DESKTOP-S18NUHB>
2025-10-22 11:13:17 +0900

    Merge branch 'master' of https://github.com/csj9630/prj1_carSell.git

--- prj1_carSell/src/kr/co/sist/ljw2/Ljw2.java
+++ prj1_carSell/src/kr/co/sist/ljw2/Ljw2.java
@@ -3,7 +3,7 @@ package kr.co.sist.ljw2;
 public class Ljw2 {
 
 	public static void main(String[] args) {
-		System.out.println("나는 이정우");
+		System.out.println("나는 이정우222");
 	}// main
 
 }// class
