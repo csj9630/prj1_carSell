d9ffc04abf3cfc03b107ca056b011eb66763737f
user <user@DESKTOP-CIDA54Q>
2025-10-22 11:39:17 +0900

    이지원 커밋입니다.

--- prj1_carSell/src/kr/co/sist/ljw/Ljw.java
+++ prj1_carSell/src/kr/co/sist/ljw/Ljw.java
@@ -3,7 +3,10 @@ package kr.co.sist.ljw;
 public class Ljw {
 
 	public static void main(String[] args) {
-		System.out.println("나는 이지원이다.");
+		System.out.println("나는 이지원이다 1트.");
+		
+		System.out.println("나는 이지원이다 2트.");
+		
 	}
 
 }
