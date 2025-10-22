cfac2c0d5d1a23d650706089f610781cf833d899
csjSist <csj9630@gmail.com>
2025-10-22 10:06:13 +0900

    프로젝트 최초 커밋

---
+++ prj1_carSell/src/kr/co/sist/csj/Csj.java
@@ -0,0 +1,9 @@
+package kr.co.sist.csj;
+
+public class Csj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("CSI Miami");
+	}//main
+}//class
