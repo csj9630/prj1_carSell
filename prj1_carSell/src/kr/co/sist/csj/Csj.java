90e52654a34d7b69f85e046f2f8ff1931ce211d2
csjSist <csj9630@gmail.com>
2025-10-22 10:28:28 +0900

    최승준 : merge 테스트

--- prj1_carSell/src/kr/co/sist/csj/Csj.java
+++ prj1_carSell/src/kr/co/sist/csj/Csj.java
@@ -4,6 +4,6 @@ public class Csj {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("CSI Miami");
+		System.out.println("CSI Miami season2");
 	}//main
 }//class
